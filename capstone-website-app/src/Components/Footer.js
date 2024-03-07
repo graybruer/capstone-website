1407b54368e3e9e495ee92ddf7fbef7f5782c09d
Gray Bruer <gbruer@gmail.com>
2024-03-07 15:33:43 -0500

    added assests, components, meta tags to index.html

---
+++ capstone-website-app/src/Components/Footer.js
@@ -0,0 +1,28 @@
+<footer>
+    <img src="" alt=""></img>
+
+    <ul>
+        <li>Doormat Navigation</li>
+        <li>Home</li>
+        <li>About</li>
+        <li>Menu</li>
+        <li>Reservations</li>
+        <li>Online Orders</li>
+        <li>Login</li>
+    </ul>
+
+    <ul>
+        <li>Contact</li>
+        <li>Address</li>
+        <li>Phone Number</li>
+        <li>Email</li>
+    </ul>
+
+    <ul>
+        <li>Social Media Links</li>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+    </ul>
+
+</footer>
---
+++ capstone-website-app/src/Components/Header.js
@@ -0,0 +1,3 @@
+<header>
+    <img src="capstone-website/icons_assets/Logo.svg" alt="Little Lemon Restaurant"></img>
+</header>
